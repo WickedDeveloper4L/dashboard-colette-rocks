b96be4c65746d683b974f957bc53835bc969dc33
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2022-12-25 00:32:03 +0100

    added the carousel to the account page to increase aesthetics

--- src/components/dashboard/dashboard pages/Account/Account.jsx
+++ src/components/dashboard/dashboard pages/Account/Account.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import './Account.scss'
 import { Outlet } from 'react-router-dom'
 import AccountActions from './Account-actions/accountActions'
+import Carousel from '../../../carousel/Carousel'
+import Logo from '../../../../images/logo_black.png'
 
 const Account = () => {
   return (
   <div className='Account_page'>
+    <div className="pages_actions">
     <AccountActions/>
     <Outlet/>
+    </div>
+    <div className='carousel'>
+    <img src={Logo} alt="logo" className='logo'/>
+    <Carousel/>
+    </div>
   </div>
   )
 }
