31b327f5d75d6c62d2d94fe0c09cd53f3c069f37
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-01-09 10:26:54 +0100

    Responsiveness for mobile view and tablets

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import {useRef} from 'react'
 import Navigation from './navigation/Navigation'
 import { Outlet } from 'react-router-dom'
 import './dashboard.scss'
+import {FaBars} from 'react-icons/fa'
+import Logo from '../../images/Logo white.png'
 
 
 const Dashboard = () => {
+
+  const navRef = useRef()
+
+  const toggleNav =()=>{
+    navRef.current.classList.toggle("responsive_nav")
+  }
   return (
     <div className='dashboard'>
+    <div className="header">
+      <FaBars className='nav_icon' onClick={toggleNav}/>
+      <img src={Logo} alt="logo" className='logo'/>
+    </div>
     <div className="dashboard_content">
-    <Navigation className='navigation'/>
+    <Navigation className='navigation' toggleNav={toggleNav} navRef={navRef}/>
     <Outlet className="flow"/>
     </div>
     </div>
--- src/components/dashboard/navigation/Navigation.jsx
+++ src/components/dashboard/navigation/Navigation.jsx
@@ -7,18 +7,19 @@ import {CgProfile} from 'react-icons/cg'
 import {FiSettings} from 'react-icons/fi'
 import Pic from '../../../pngs/Ellipse7.png'
 import Logo from '../../../images/Logo white.png'
+import {FaTimes} from 'react-icons/fa'
 
-const Navigation = () => {
+const Navigation = ({navRef, toggleNav}) => {
 
 
   return (
-    <div className='navigation'>
+    <div className='navigation' ref={navRef}>
     <div className="profile">
     <img src={Pic} alt="pic" className='pic'/>
     <h2 className="name">John Doe</h2>
     <h3 className="email">johndoe@gmail.com</h3>
     </div>
-    <div className="links">
+    <div className="links" onClick={toggleNav}>
     <NavLink className={({isActive}) => isActive ? "Link-active link" : "Link-inactive link"} to='/dashboard/Account/Deposit'>
     <TfiWallet className='icon'/>My Wallet</NavLink>
     <NavLink className={({isActive}) => isActive ? "Link-active link" : "Link-inactive link"} to='/dashboard/Transactions'><RiExchangeDollarFill className='icon'/>Transactions</NavLink>
@@ -26,6 +27,7 @@ const Navigation = () => {
     <NavLink className={({isActive}) => isActive ? "Link-active link" : "Link-inactive link"} to='/dashboard/settings'><FiSettings className='icon'/>Settings</NavLink>
     </div>
       <img src={Logo} alt="colette-rocks" className='logo'/>
+      <FaTimes className='close_btn' onClick={toggleNav}/>
     </div>
   )
 }
